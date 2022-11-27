745ab27c3dba4f7e31340c417ca55f8c6f4ec19b
Andrew Brighton <snatvb@ya.ru>
2022-11-27 02:45:20 +0200

    Added mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { validationSchema } from './config'
 import { VerificationModule } from './verification/verification.module'
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter'
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer'
 
 @Module({
   imports: [
@@ -24,6 +26,32 @@ import { VerificationModule } from './verification/verification.module'
       envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
       isGlobal: true,
       validationSchema,
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      async useFactory(config: ConfigService): Promise<MailerOptions> {
+        return {
+          transport: {
+            service: config.getOrThrow<string>('MAILER_SERVICE'),
+            host: config.getOrThrow<string>('MAILER_HOST'),
+            auth: {
+              user: config.getOrThrow<string>('MAILER_USER'),
+              pass: config.getOrThrow<string>('MAILER_PASSWORD'),
+            },
+          },
+          defaults: {
+            from: config.getOrThrow<string>('MAILER_FROM'),
+          },
+          template: {
+            dir: __dirname + '/templates',
+            adapter: new PugAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        }
+      },
     }),
     UsersModule,
     PrismaModule,
--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,23 @@ import * as joi from 'joi'
 
 export const validationSchema = joi.object({
   NODE_ENV: joi.string().valid('development', 'production', 'test'),
+  PORT: joi.number().default(3000),
+
   JWT_AT_SECRET: joi.string().required(),
   JWT_EXPIRES: joi.string().required(),
   JWT_EMAIL_SECRET: joi.string().required(),
   JWT_EMAIL_EXPIRES: joi.string().required(),
   JWT_RECOVERY_SECRET: joi.string().required(),
   JWT_RECOVERY_EXPIRES: joi.string().required(),
-  PORT: joi.number().default(3000),
+
   POSTGRES_PASSWORD: joi.string().required(),
   POSTGRES_USER: joi.string().required(),
   POSTGRES_DB: joi.string().required(),
   DATABASE_URL: joi.string().required(),
+
+  MAILER_SERVICE: joi.string().required(),
+  MAILER_HOST: joi.string().required(),
+  MAILER_USER: joi.string().required(),
+  MAILER_PASSWORD: joi.string().required(),
+  MAILER_FROM: joi.string().required(),
 })
